3751700c77028ebd7e358e80bf8de2f4be3fc0f8
Michael Tonini <michael.jack.tonini@gmail.com>
2020-07-03 15:17:07 +1000

    settingup pipeline

--- MeterServer/src/main/java/com/zodicom/App.java
+++
@@ -1,9 +0,0 @@
-package com.zodicom;
-
-public class App {
-  /*  public static void main(String args[]) {
-        System.out.println("Welcome to Zodicom");
-        System.out.println("Zodicom application started");
-    }
-    */
-}
--- MeterServer/testThreadPool.py
+++ MeterServer/testThreadPool.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+12#!/usr/bin/env python
  
 import socket
 import threading
