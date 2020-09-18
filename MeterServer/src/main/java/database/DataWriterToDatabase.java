cf94e334938964b073f1b835d423f3327d327dae
Michael Tonini <michael.jack.tonini@gmail.com>
2020-09-18 17:53:38 +1000

    fixed noClassDefFoun error

--- MeterServer/src/main/java/database/DataWriterToDatabase.java
+++ MeterServer/src/main/java/database/DataWriterToDatabase.java
@@ -1,9 +1,6 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 import socketserver.ServerToDatabase;
